1f29d9e17ca1fd10c8c4b1682c7e53e4d6e668cf
qinhj <qinhj@lsec.cc.ac.cn>
2021-06-17 12:37:24 +0800

    [data] bug fix: typo

--- data/VOC0712/create_data.sh
+++ data/VOC0712/create_data.sh
@@ -10,13 +10,11 @@ set -e # -xe
 caffe_dir=$HOME/desktop/caffe
 cur_dir=$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
 
-#cd $caffe_dir
+data_dir="$HOME/data/VOCdevkit"
+out_dir_lmdb=$cur_dir/VOC0712 #$data_dir/VOC0712
+out_dir_link=$cur_dir
 
 redo=1
-
-data_dir="$HOME/data/VOCdevkit"
-#dataset_name="VOC0712"
-
 mapfile="$cur_dir/labelmap_voc.prototxt"
 anno_type="detection"
 db="lmdb"
@@ -30,26 +28,26 @@ if [ $redo ]; then
   extra_cmd="$extra_cmd --redo"
 fi
 
-## remove exist softlink
-[ -d examples/$dataset_name ] && rm -rf examples/$dataset_name
-
 for subset in test trainval; do
-# =====================================================================
-# usage: create_annoset.py [-h] [--redo] [--anno-type ANNO_TYPE]
-#                          [--label-type LABEL_TYPE] [--backend BACKEND]
-#                          [--check-size] [--encode-type ENCODE_TYPE]
-#                          [--encoded] [--gray]
-#                          [--label-map-file LABEL_MAP_FILE] [--min-dim MIN_DIM]
-#                          [--max-dim MAX_DIM] [--resize-height RESIZE_HEIGHT]
-#                          [--resize-width RESIZE_WIDTH] [--shuffle]
-#                          [--check-label]
-#                          root listfile outdir exampledir
-# where
-#   root          as dataset root dir
-#   listfile      as trainval.txt/test.txt
-#   outdir        as output "db" file path
-#   exampledir    as output dir with softlink
-# =====================================================================
+  ## remove exist softlink
+  rm -rf $out_dir_link/$subset"_"$db
+
+  # =====================================================================
+  # usage: create_annoset.py [-h] [--redo] [--anno-type ANNO_TYPE]
+  #                          [--label-type LABEL_TYPE] [--backend BACKEND]
+  #                          [--check-size] [--encode-type ENCODE_TYPE]
+  #                          [--encoded] [--gray]
+  #                          [--label-map-file LABEL_MAP_FILE] [--min-dim MIN_DIM]
+  #                          [--max-dim MAX_DIM] [--resize-height RESIZE_HEIGHT]
+  #                          [--resize-width RESIZE_WIDTH] [--shuffle]
+  #                          [--check-label]
+  #                          root listfile outdir exampledir
+  # where
+  #   root          as dataset root dir
+  #   listfile      as trainval.txt/test.txt
+  #   outdir        as output "db" file path
+  #   exampledir    as output dir with softlink
+  # =====================================================================
   python $caffe_dir/scripts/create_annoset.py \
     --anno-type=$anno_type \
     --label-map-file=$mapfile \
@@ -58,6 +56,6 @@ for subset in test trainval; do
     --check-label $extra_cmd \
     $data_dir \
     $cur_dir/$subset.txt \
-    $cur_dir/$db/$subset"_"$db \
-    $cur_dir/examples
+    $out_dir_lmdb/$subset"_"$db \
+    $out_dir_link
 done
